dcf3a012471bcc93349329f0c130b2a91b443b3b
mengjia <cmengj02@gmail.com>
2018-09-19 14:29:23 +1000

    Update Payload.java

--- src/test/java/files/Payload.java
+++ src/test/java/files/Payload.java
@@ -45,7 +45,7 @@ public class Payload {
 		String body = "{\n" +
 				"\t\"name\":\"Trump\",\n" +
 				"\t\"gender\": \"F\",\n" +
-				"\t\"Hobby\": \"Money\"\n" +
+				"\t\"hobby\": \"Money\"\n" +
 				"}";
 		return body;
 	}
